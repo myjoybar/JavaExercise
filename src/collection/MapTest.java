a5cc5a3a89d1772ab749010fdc7ca24b121c2045
joybar <myjoybar@gmail.com>
2017-05-27 17:20:39 +0800

    Add test currentTimeMillis

--- src/collection/MapTest.java
+++ src/collection/MapTest.java
@@ -16,6 +16,8 @@ public class MapTest {
     Map<User, User> map = new HashMap<>();
 
     public static void main(String[] args) {
+
+
         System.out.println("Hello World!");
         MapTest test = new MapTest();
         test.initMap();
@@ -26,6 +28,14 @@ public class MapTest {
 
 
     private void initMap() {
+
+        long time1 = System.currentTimeMillis();
+
+        try {
+            Thread.sleep(1*1000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
         User user1 = new User("Tom", 11);
         User user2 = new User("Jim", 11);
         map.put(user1, user1);
@@ -33,9 +43,11 @@ public class MapTest {
 
         User user3 = new User("Jim", 11);
 
-        System.out.println(map.get(user3).toString());
+      //  System.out.println(map.get(user3).toString());
 
+        long time2 = System.currentTimeMillis();
 
+        System.out.println(time2-time1);
     }
 
 }
